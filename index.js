3fc46e65ffb8a47abb904c596a9da5b79975fa20
Moon <108756201+LifeAdventurer@users.noreply.github.com>
2023-10-08 17:03:14 +0800

    Update the clock as dynamic

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 let week = ['日', '一', '二', '三', '四', '五', '六'];
-let date = new Date();
-let Y = date.getFullYear();
-let M = date.getMonth() + 1;
-let D = date.getDate();
-let h = date.getHours();
-let m = date.getMinutes();
-let s = date.getSeconds();
-
 
 const update = {
   data() {
     return {
-      date: Y + "." + M + "." + ("0" + D).substr(-2) + " (" + week[date.getDay()] + ")",
-      time: ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2)
-    };
+      date: ' ',
+      time: ' '
+    }
+  },    
+  mounted() {
+    setInterval(() => {
+      let date = new Date();
+      return this.date = date.getFullYear() + "." + (date.getMonth() + 1) + "." + ("0" + date.getDate()).substr(-2) + " (" + week[date.getDay()] + ")";
+    }, 1000);
+    setInterval(() => {
+      let date = new Date();
+      return this.time = ("0" + date.getHours()).substr(-2) + ":" + ("0" + date.getMinutes()).substr(-2) + ":" + ("0" + date.getSeconds()).substr(-2);
+    }, 1000);
   },
 }
 Vue.createApp(update).mount('#clock');
