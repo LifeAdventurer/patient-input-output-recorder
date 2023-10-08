0b4e9e240f6ff0b4f36b2c3c52460afa4a89de82
Moon <108756201+LifeAdventurer@users.noreply.github.com>
2023-10-08 14:30:03 +0800

    Update index.js using Vue

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-let DATE = document.getElementById("DATE"); 
-let TIME = document.getElementById("TIME"); 
+let week = ['日', '一', '二', '三', '四', '五', '六'];
+let date = new Date();
+let Y = date.getFullYear();
+let M = date.getMonth() + 1;
+let D = date.getDate();
+let h = date.getHours();
+let m = date.getMinutes();
+let s = date.getSeconds();
 
-function clock() {
-  let date = new Date();
-  let Y = date.getFullYear();
-  let M = date.getMonth() + 1;
-  let D = date.getDate();
-  let h = date.getHours();
-  let m = date.getMinutes();
-  let s = date.getSeconds();
-  DATE.textContent = Y + "-" + M + "-" + ("0" + D).substr(-2);
-  TIME.textContent = ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+
+const update = {
+  data() {
+    return {
+      date: Y + "." + M + "." + ("0" + D).substr(-2) + " (" + week[date.getDay()] + ")",
+      time: ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2)
+    };
+  },
 }
-
-setInterval(clock, 1000)
+Vue.createApp(update).mount('#clock');
