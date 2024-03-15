e6019c2f7caf141958c79ec3f01605df7351f3bb
lifeadventurer <life0adventurer@gmail.com>
2024-03-15 21:44:00 +0800

    feat: Add dynamic generation of options using Vue.js v-for directive

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ Vue.createApp({
       date: ' ',
       time: ' ',
       // record bar content
+      options: [
+        { value: 50, label: '50' },
+        { value: 100, label: '100' },
+        { value: 150, label: '150' },
+        { value: 200, label: '200' },
+        { value: 250, label: '250' },
+        { value: 300, label: '300' },
+        { value: 350, label: '350' },
+        { value: 400, label: '400' }
+      ],
       inputFood: 0,
       inputWater: 0,
       inputUrination: 0,
