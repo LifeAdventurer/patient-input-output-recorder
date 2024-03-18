a08fb7321d81453a9eb9145e14cf2b16b16ff2c9
lifeadventurer <life0adventurer@gmail.com>
2024-03-18 15:50:28 +0800

    Feat: Implement backend/gen_qr_code.py for generating QR code from provided account and password
    The generated QR code can automatically pass the authentication of patient side

---
+++ backend/gen_qr_code.py
@@ -0,0 +1,25 @@
+import qrcode
+
+
+def generate_qr_code(data, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+
+
+if __name__ == "__main__":
+
+    account = input("Enter the account: ")
+    password = input("Enter the password:　")
+    url = f"https://lifeadventurer.github.io/patient-diet-recorder/patient/?acct={account}&pw={password}"
+    filename = f"./qr_code_acct_{account}_pw_{password}.png"
+    generate_qr_code(url, filename)
+    print(f"QR code saved as {filename}")
