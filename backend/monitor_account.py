ff4dec3d16fd9d47dbddae1d5530c2c5be013db5
lifeadventurer <life0adventurer@gmail.com>
2024-03-24 01:57:16 +0800

    Feat: Implement 'monitor_account.py' to add patient accounts to the monitoring list of monitor accounts

---
+++ backend/monitor_account.py
@@ -0,0 +1,41 @@
+import json
+
+import db
+
+ACCOUNT = input("Enter the monitor account: ")
+PASSWORD = input("Enter the password: ")
+
+if db.authenticate(ACCOUNT, PASSWORD):
+    if db.get_account_type(ACCOUNT) in [
+        db.AccountType.MONITOR,
+        db.AccountType.ADMIN,
+    ]:
+        with open('./account_relations.json', 'r') as f:
+            account_relations = json.load(f)
+
+        if ACCOUNT not in account_relations['monitor_accounts']:
+            account_relations['monitor_accounts'][ACCOUNT] = []
+        print(
+            "Start entering the accounts you want to monitor: \n  (enter 'end' to stop task)"
+        )
+        while True:
+            patient_account = input()
+            if patient_account == 'end':
+                break
+
+            patient_account_type = db.get_account_type(patient_account)
+            if patient_account_type is None:
+                print("No such account")
+            elif patient_account_type == db.AccountType.PATIENT:
+                account_relations['monitor_accounts'][ACCOUNT].append(
+                    patient_account
+                )
+            else:
+                print(f"Account: '{patient_account}' is not a PATIENT")
+        with open('./account_relations.json', 'w') as f:
+            json.dump(account_relations, f, indent=4)
+        print("Task ended")
+    else:
+        print("Incorrect account type")
+else:
+    print("Unauthorized")
