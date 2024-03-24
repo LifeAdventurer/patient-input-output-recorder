3c27f184cff3a0ad57e7d0b10abc6eee6c0feea7
lifeadventurer <life0adventurer@gmail.com>
2024-03-24 23:52:44 +0800

    Feat: Update gen_qr_code.py to generate QR code for both patient/ and monitor/

--- backend/gen_qr_code.py
+++ backend/gen_qr_code.py
@@ -3,20 +3,37 @@ import qrcode
 
 def generate_qr_code(account, password, account_type):
 
-    url = f"https://lifeadventurer.github.io/patient-diet-recorder/patient/?acct={account}&pw={password}"
-    filename = f"./account_qr_codes/{account_type}/qr_code_acct_{account}_pw_{password}.png"
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(url)
-    qr.make(fit=True)
+    if account_type != 'MONITOR':
+        url = f"https://lifeadventurer.github.io/patient-diet-recorder/patient/?acct={account}&pw={password}"
+        filename = f"./account_qr_codes/{account_type}/qr_code_acct_{account}_pw_{password}.png"
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(url)
+        qr.make(fit=True)
 
-    img = qr.make_image(fill_color="black", back_color="white")
-    img.save(filename)
-    print(f"QR code saved as '{filename}'")
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save(filename)
+        print(f"QR code saved as '{filename}'")
+
+    if account_type != 'PATIENT':
+        url = f"https://lifeadventurer.github.io/patient-diet-recorder/monitor/?acct={account}&pw={password}"
+        filename = f"./account_qr_codes/{account_type}/qr_code_acct_{account}_pw_{password}.png"
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(url)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save(filename)
+        print(f"QR code saved as '{filename}'")
 
 
 if __name__ == "__main__":
