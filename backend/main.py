156505d8cdb85136c82759634962cac928b13079
lifeadventurer <life0adventurer@gmail.com>
2024-04-03 23:17:43 +0800

    Refactor: Refactor all code in main.py

--- backend/main.py
+++ backend/main.py
@@ -24,115 +24,129 @@ app.add_middleware(
 # Constants
 DATA_JSON_PATH = './data.json'
 ACCT_REL_JSON_PATH = './account_relations.json'
+CONFIG_JSON_PATH = './config.json'
+
+
+def load_json_file(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+
+def write_json_file(file_path, data):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
 
 
 @app.post("/")
-async def write_data(post_request: Request):
+async def handle_request(post_request: Request):
     post_request = await post_request.json()
+    event = post_request.get('event')
 
-    with open('./config.json', 'r') as f:
-        token = json.load(f)['token']
+    if event in [
+        'sign up',
+        'del account',
+        'change password',
+        'fetch account list',
+    ]:
+        return handle_authenticated_request(post_request)
 
+    elif event in [
+        'update record',
+        'update patient record from monitor',
+        'fetch patient records',
+        'fetch monitoring account records',
+        'add patient account to monitoring list',
+    ]:
+        return handle_request_without_authentication(post_request)
+
+    else:
+        return {"message": "Invalid event"}
+
+
+def handle_authenticated_request(post_request):
+    token = load_json_file(CONFIG_JSON_PATH).get('token')
+
+    if not token or post_request.get('token') != token:
+        return {"message": "Incorrect token"}
+
+    return handle_request_without_authentication(post_request)
+
+
+def handle_request_without_authentication(post_request):
     if post_request['event'] == 'sign up':
-        if post_request['token'] == token:
-            if post_request['account_type'] not in [
-                db.AccountType.ADMIN,
-                db.AccountType.PATIENT,
-                db.AccountType.MONITOR,
-            ]:
-                return {"message": "Incorrect account type"}
+        if post_request['account_type'] not in [
+            db.AccountType.ADMIN,
+            db.AccountType.PATIENT,
+            db.AccountType.MONITOR,
+        ]:
+            return {"message": "Incorrect account type"}
 
-            err = db.add_account(
-                post_request['account'],
-                post_request['password'],
-                post_request['account_type'],
-            )
-            if err is not None:
-                return {"message": "Account already exists"}
+        err = db.add_account(
+            post_request['account'],
+            post_request['password'],
+            post_request['account_type'],
+        )
+        if err is not None:
+            return {"message": "Account already exists"}
 
-            with open(DATA_JSON_PATH, 'r') as f:
-                data = json.load(f)
+        data = load_json_file(DATA_JSON_PATH)
+        data[post_request['account']] = {}
+        write_json_file(DATA_JSON_PATH, data)
 
-            data[post_request['account']] = {}
+        return {"message": "Account created successfully"}
 
-            with open(DATA_JSON_PATH, 'w') as f:
-                json.dump(data, f, indent=4)
+    elif post_request['event'] == 'fetch account list':
+        account_list = db.get_all_accounts()
+        return {"message": "Fetch Success", "account_list": account_list}
 
-            return {"message": "Account created successfully"}
-        else:
-            return {"message": "Incorrect token"}
+    err = db.authenticate(post_request['account'], post_request['password'])
+    if err != "Authentication successful":
+        return {"message": err}
 
-    elif post_request['event'] == 'del account':
-        if post_request['token'] == token:
-            err = db.authenticate(
-                post_request['account'], post_request['password']
-            )
-            account_type = db.get_account_type(post_request['account'])
-            if err != "Authentication successful":
-                return {"message": err}
+    if post_request['event'] == 'del account':
+        account_type = db.get_account_type(post_request['account'])
+        err = db.delete_account(post_request['account'])
+        if err is not None:
+            return {"message": "Account does not exists"}
 
-            err = db.delete_account(post_request['account'])
-            if err is not None:
-                return {"message": "Account does not exists"}
+        # Remove deleted account data in JSON files
+        if account_type in [
+            db.AccountType.PATIENT,
+            db.AccountType.ADMIN,
+        ]:
+            # account_relations.json
+            account_relations = load_json_file(ACCT_REL_JSON_PATH)
+            for monitor_account, patient_accounts in account_relations[
+                'monitor_accounts'
+            ].items():
+                if post_request['account'] in patient_accounts:
+                    del account_relations['monitor_accounts'][monitor_account][
+                        patient_accounts.index(post_request['account'])
+                    ]
+            write_json_file(ACCT_REL_JSON_PATH, account_relations)
 
-            # Remove deleted account data in JSON files
-            if account_type in [
-                db.AccountType.PATIENT,
-                db.AccountType.ADMIN,
-            ]:
-                # account_relations.json
-                with open(ACCT_REL_JSON_PATH, 'r') as f:
-                    account_relations = json.load(f)
+            # data.json
+            data = load_json_file(DATA_JSON_PATH)
+            del data[post_request['account']]
+            write_json_file(DATA_JSON_PATH, data)
 
-                for monitor_account, patient_accounts in account_relations[
-                    'monitor_accounts'
-                ].items():
-                    if post_request['account'] in patient_accounts:
-                        del account_relations['monitor_accounts'][
-                            monitor_account
-                        ][patient_accounts.index(post_request['account'])]
+        if account_type in [db.AccountType.MONITOR, db.AccountType.ADMIN]:
+            account_relations = load_json_file(ACCT_REL_JSON_PATH)
 
-                with open(ACCT_REL_JSON_PATH, 'w') as f:
-                    json.dump(account_relations, f, indent=4)
+            if (
+                post_request['account']
+                in account_relations['monitor_accounts'].keys()
+            ):
+                del account_relations['monitor_accounts'][monitor_account]
 
-                # data.json
-                with open(DATA_JSON_PATH, 'r') as f:
-                    data = json.load(f)
+            write_json_file(ACCT_REL_JSON_PATH, account_relations)
 
-                del data[post_request['account']]
-
-                with open(DATA_JSON_PATH, 'w') as f:
-                    json.dump(data, f, indent=4)
-
-            if account_type in [db.AccountType.MONITOR, db.AccountType.ADMIN]:
-                with open(ACCT_REL_JSON_PATH, 'r') as f:
-                    account_relations = json.load(f)
-
-                if (
-                    post_request['account']
-                    in account_relations['monitor_accounts'].keys()
-                ):
-                    del account_relations['monitor_accounts'][monitor_account]
-
-                with open(ACCT_REL_JSON_PATH, 'w') as f:
-                    json.dump(account_relations, f, indent=4)
-
-            return {
-                "message": "Account deleted successfully",
-                "account_type": account_type,
-            }
-        else:
-            return {"message": "Incorrect token"}
+        return {
+            "message": "Account deleted successfully",
+            "account_type": account_type,
+        }
 
     elif post_request['event'] == 'change password':
-        if post_request['token'] != token:
-            return {"message": "Incorrect token"}
-
-        err = db.authenticate(post_request['account'], post_request['password'])
-
-        if err != "Authentication successful":
-            return {"message": err}
-
         db.change_account_password(
             post_request['account'], post_request['changed_password']
         )
@@ -142,116 +156,75 @@ async def write_data(post_request: Request):
             "account_type": db.get_account_type(post_request['account']),
         }
 
-    elif post_request['event'] == 'fetch account list':
-        if post_request['token'] == token:
-            account_list = db.get_all_accounts()
-            return {"message": "Fetch Success", "account_list": account_list}
-        else:
-            return {"message": "Incorrect token"}
-
     elif post_request['event'] == 'update record':
-        record = post_request
-        err = db.authenticate(post_request['account'], post_request['password'])
-        if err != "Authentication successful":
-            return {"message": err}
-
-        with open(DATA_JSON_PATH, 'r') as f:
-            account_records = json.load(f)
-
-        account_records[record['account']] = record['data']
-        with open(DATA_JSON_PATH, 'w') as f:
-            json.dump(account_records, f, indent=4)
+        data = load_json_file(DATA_JSON_PATH)
+        data[post_request['account']] = post_request['data']
+        write_json_file(DATA_JSON_PATH, data)
 
         return {"message": "Update Success"}
 
     elif post_request['event'] == 'update patient record from monitor':
-        err = db.authenticate(post_request['account'], post_request['password'])
-        if err != "Authentication successful":
-            return {"message": err}
-
         if (
             db.get_account_type(post_request['account'])
             == db.AccountType.PATIENT
         ):
             return {"message": "Incorrect account type"}
 
-        with open(DATA_JSON_PATH, 'r') as f:
-            account_records = json.load(f)
-
-        account_records[post_request['patient_account']] = post_request['data']
-        with open(DATA_JSON_PATH, 'w') as f:
-            json.dump(account_records, f, indent=4)
+        data = load_json_file(DATA_JSON_PATH)
+        data[post_request['patient_account']] = post_request['data']
+        write_json_file(DATA_JSON_PATH, data)
 
         return {"message": "Update Success"}
 
     elif post_request['event'] == 'fetch patient records':
-        err = db.authenticate(post_request['account'], post_request['password'])
-        if err != "Authentication successful":
-            return {"message": err}
-
         patient_account = post_request['account']
         if db.get_account_type(patient_account) in [
             db.AccountType.PATIENT,
             db.AccountType.ADMIN,
         ]:
-            with open(DATA_JSON_PATH, 'r') as f:
-                data = json.load(f)
-                return {
-                    "message": "Fetch Success",
-                    "account_records": data[patient_account],
-                }
+            data = load_json_file(DATA_JSON_PATH)
+            return {
+                "message": "Fetch Success",
+                "account_records": data[patient_account],
+            }
         else:
             return {"message": "Incorrect account type"}
 
     elif post_request['event'] == 'fetch monitoring account records':
-        err = db.authenticate(post_request['account'], post_request['password'])
-        if err != "Authentication successful":
-            return {"message": err}
-
         monitoring_account = post_request['account']
         if db.get_account_type(monitoring_account) in [
             db.AccountType.MONITOR,
             db.AccountType.ADMIN,
         ]:
-            with open(ACCT_REL_JSON_PATH, 'r') as f:
-                account_relations = json.load(f)
-                if monitoring_account in account_relations['monitor_accounts']:
-                    patient_accounts = account_relations['monitor_accounts'][
-                        monitoring_account
-                    ]
-                    with open(DATA_JSON_PATH, 'r') as f:
-                        data = json.load(f)
-                    patient_records = {}
-                    for patient_account in patient_accounts:
-                        if patient_account not in data:
-                            patient_records[patient_account] = {}
-                        else:
-                            patient_records[patient_account] = data[
-                                patient_account
-                            ]
+            account_relations = load_json_file(ACCT_REL_JSON_PATH)
+            if monitoring_account in account_relations['monitor_accounts']:
+                patient_accounts = account_relations['monitor_accounts'][
+                    monitoring_account
+                ]
+                data = load_json_file(DATA_JSON_PATH)
+                patient_records = {}
+                for patient_account in patient_accounts:
+                    if patient_account not in data:
+                        patient_records[patient_account] = {}
+                    else:
+                        patient_records[patient_account] = data[patient_account]
 
-                    return {
-                        "message": "Fetch Success",
-                        "patient_accounts": patient_accounts,
-                        "patient_records": patient_records,
-                    }
-                else:
-                    return {"message": "No associated patient accounts"}
+                return {
+                    "message": "Fetch Success",
+                    "patient_accounts": patient_accounts,
+                    "patient_records": patient_records,
+                }
+            else:
+                return {"message": "No associated patient accounts"}
         else:
             return {"message": "Incorrect account type"}
 
     elif post_request['event'] == 'add patient account to monitoring list':
         monitor_account = post_request['account']
-        err = db.authenticate(post_request['account'], post_request['password'])
-        if err != "Authentication successful":
-            return {"message": err}
-
         if db.get_account_type(monitor_account) == db.AccountType.PATIENT:
             return {"message": "Incorrect account type"}
 
-        with open('./account_relations.json', 'r') as f:
-            account_relations = json.load(f)
-
+        account_relations = load_json_file(ACCT_REL_JSON_PATH)
         if monitor_account not in account_relations['monitor_accounts']:
             account_relations['monitor_accounts'][monitor_account] = []
 
@@ -259,6 +232,7 @@ async def write_data(post_request: Request):
         patient_account_type = db.get_account_type(patient_account)
         if patient_account_type is None:
             return {"message": "No such account"}
+
         elif patient_account_type == db.AccountType.PATIENT:
             if (
                 patient_account
@@ -269,8 +243,8 @@ async def write_data(post_request: Request):
                 )
                 account_relations['monitor_accounts'][monitor_account].sort()
 
-                with open('./account_relations.json', 'w') as f:
-                    json.dump(account_relations, f, indent=4)
+                write_json_file(ACCT_REL_JSON_PATH, account_relations)
+
             return {"message": "Added"}
         else:
             return {"message": f"Account: '{patient_account}' is not a PATIENT"}
