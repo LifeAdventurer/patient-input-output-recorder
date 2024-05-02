62f62161b2e7c54bfee2a38a801d3fa6d7b44c75
lifeadventurer <life0adventurer@gmail.com>
2024-05-02 13:31:57 +0800

    Impr(backend): Also deletes the QR code if the account does not exists

--- backend/del_account.py
+++ backend/del_account.py
@@ -23,14 +23,16 @@ try:
     response = requests.post(URL, json=payload, headers=headers)
     response.raise_for_status()
     response = response.json()
-    if response['message'] == 'Account deleted successfully':
-        print('Account deleted successfully')
+    print(response['message'])
+    if (
+        response['message'] == 'Account deleted successfully'
+        or response['message'] == 'Account does not exists'
+    ):
         ACCOUNT_TYPE = response['account_type']
         QR_code_path = f"./account_qr_codes/{ACCOUNT_TYPE}/qr_code_acct_{ACCOUNT}_pw_{PASSWORD}.png"
         if os.path.exists(QR_code_path):
             os.remove(QR_code_path)
             print(f"'{QR_code_path}' has been successfully deleted.")
-    else:
-        print(response['message'])
+
 except requests.exceptions.RequestException as e:
     print('Failed to post data:', e)
